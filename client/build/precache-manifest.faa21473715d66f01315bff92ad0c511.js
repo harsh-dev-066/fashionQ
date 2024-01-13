08acb164be46984dcda82225f58b2c497547afd1
harsh-dev-066 <harshthakur066@gmail.com>
2024-01-13 18:06:55 +0530

    added build

---
+++ client/build/precache-manifest.faa21473715d66f01315bff92ad0c511.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff0309857858ad3879a04be676589c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfa7ff81834fb0ecf08f",
+    "url": "/static/css/4.8da0576e.chunk.css"
+  },
+  {
+    "revision": "f2aed41b109a491d44a4",
+    "url": "/static/css/5.f53bbd1e.chunk.css"
+  },
+  {
+    "revision": "e369b9f39c2549776e28",
+    "url": "/static/css/6.1591e0c3.chunk.css"
+  },
+  {
+    "revision": "4ab91badb6d4000db778",
+    "url": "/static/css/7.3d852a35.chunk.css"
+  },
+  {
+    "revision": "4b3958a79121f301a948",
+    "url": "/static/css/8.1c498379.chunk.css"
+  },
+  {
+    "revision": "db3a1aa27a687dd235eb",
+    "url": "/static/css/main.9ce49e0e.chunk.css"
+  },
+  {
+    "revision": "01de23f620cfed997ef3",
+    "url": "/static/js/2.b405a2fc.chunk.js"
+  },
+  {
+    "revision": "d7e164bc880d25710db675f58f847190",
+    "url": "/static/js/2.b405a2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7852abc9d973a544539e",
+    "url": "/static/js/3.332ea2b1.chunk.js"
+  },
+  {
+    "revision": "dfa7ff81834fb0ecf08f",
+    "url": "/static/js/4.d0cb342e.chunk.js"
+  },
+  {
+    "revision": "f2aed41b109a491d44a4",
+    "url": "/static/js/5.9e92c293.chunk.js"
+  },
+  {
+    "revision": "e369b9f39c2549776e28",
+    "url": "/static/js/6.58fbc9c5.chunk.js"
+  },
+  {
+    "revision": "4ab91badb6d4000db778",
+    "url": "/static/js/7.7fecbd3d.chunk.js"
+  },
+  {
+    "revision": "4b3958a79121f301a948",
+    "url": "/static/js/8.d022ad8a.chunk.js"
+  },
+  {
+    "revision": "db3a1aa27a687dd235eb",
+    "url": "/static/js/main.ab1ae556.chunk.js"
+  },
+  {
+    "revision": "6d8135815fcdb4f54865",
+    "url": "/static/js/runtime-main.1c4528f6.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/logo.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
